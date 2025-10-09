fe5cbca23530e11f3de667b653e28ab6d54e7832
Dogu888 <157326953+Dogu888@users.noreply.github.com>
2025-10-09 11:29:51 +0300

    ÖDEV 3

---
+++ WindowsFormsApp091025DKKO/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp091025DKKO
+{
+    public partial class Form1 : Form
+    {
+        private Point btn1Start, btn2Start, btn3Start, btn4Start;
+        private Point btn1Target, btn2Target, btn3Target, btn4Target;
+        private Timer animationTimer;
+        private int step = 0;
+        private bool goingToCenter = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            btn1Start = button1.Location;
+            btn2Start = button2.Location;
+            btn3Start = button3.Location;
+            btn4Start = button4.Location;
+
+           
+            int centerX = (this.ClientSize.Width - button1.Width) / 2;
+            int centerY = (this.ClientSize.Height - button1.Height) / 2;
+
+            int half = button1.Width / 2; 
+
+            btn1Target = new Point(centerX - half, centerY - button1.Height - 2); 
+            btn2Target = new Point(centerX - half, centerY + button1.Height + 2 - button1.Height); 
+            btn3Target = new Point(centerX + half, centerY - button1.Height - 2); 
+            btn4Target = new Point(centerX + half, centerY + button1.Height + 2 - button1.Height); 
+
+            
+            animationTimer = new Timer();
+            animationTimer.Interval = 20;
+            animationTimer.Tick += AnimationTimer_Tick;
+            animationTimer.Start();
+        }
+
+        private void AnimationTimer_Tick(object sender, EventArgs e)
+        {
+            step++;
+            int totalSteps = 50;
+            float t = (float)step / totalSteps;
+
+            if (goingToCenter)
+            {
+                button1.Location = Lerp(btn1Start, btn1Target, t);
+                button2.Location = Lerp(btn2Start, btn2Target, t);
+                button3.Location = Lerp(btn3Start, btn3Target, t);
+                button4.Location = Lerp(btn4Start, btn4Target, t);
+
+                if (step >= totalSteps)
+                {
+                    step = 0;
+                    goingToCenter = false;
+                }
+            }
+            else
+            {
+                button1.Location = Lerp(btn1Target, btn1Start, t);
+                button2.Location = Lerp(btn2Target, btn2Start, t);
+                button3.Location = Lerp(btn3Target, btn3Start, t);
+                button4.Location = Lerp(btn4Target, btn4Start, t);
+
+                if (step >= totalSteps)
+                {
+                    step = 0;
+                    goingToCenter = true;
+                }
+            }
+        }
+
+        private Point Lerp(Point start, Point end, float t)
+        {
+            if (t > 1f) t = 1f;
+            int x = (int)(start.X + (end.X - start.X) * t);
+            int y = (int)(start.Y + (end.Y - start.Y) * t);
+            return new Point(x, y);
+        }
+    }
+}
---
+++ WindowsFormsApp091025DKKO/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("WindowsFormsApp091025DKKO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsApp091025DKKO")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("2c400532-b273-4764-848c-2cb339de18fc")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
